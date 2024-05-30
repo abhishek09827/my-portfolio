674a72739f39dd79cf7b87c3da8c7b28dd1c4b83
abhishek09827 <107637702+abhishek09827@users.noreply.github.com>
2024-05-30 22:43:17 +0530

    My Portfolio

---
+++ data/index.ts
@@ -0,0 +1,208 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Driven. Creative. Always learning. ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Real-time code collaboration platform",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "NextGenCareers",
+    des: " A Career Hub, a cutting-edge platform revolutionizing talent acquisition with AI-assisted resume screening and a Professional Empowerment Hub for Women",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/th.svg", "/no.svg"],
+    link: "https://github.com/Divyansh-0/sickUI",
+  },
+  {
+    id: 2,
+    title: "RelicAI",
+    des: "AI-powered virtual tour guides featuring personalized navigation, AI voice generators, and dynamic ugmented reality visualizations using a hybrid GANs model",
+    img: "/p2.svg",
+    iconLists: ["/fl.svg", "/fi.svg"],
+    link: "https://drive.google.com/drive/folders/1EXWz6WbDGMIMNp2VrFTCRN9neLEHvxS_",
+  },
+  {
+    id: 3,
+    title: "Farm Financer",
+    des: "AI-driven farming app, addressing challenges of user adoption and diverse linguistic needs while streamlining financial tracking and providing personalized guidance.",
+    img: "/p3.svg",
+    iconLists: ["/fl.svg", "/fi.svg"],
+    link: "https://devfolio.co/projects/farmfinancer-0c02",
+  },
+  {
+    id: 4,
+    title: "KSPolice Help",
+    des: "Developed and implemented AI algorithms for real-time crime prediction and proactive crime forecasting, enhancing law enforcement s ability to prevent and respond to criminal activities.",
+    img: "/p4.svg",
+    iconLists: ["/py.svg", "/fi.svg"],
+    link: "https://ksp-hackinfinities.streamlit.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "This AI-powered mobile app streamlines financial tracking and management, offers personalized guidance, and enhances access to vital information and resources, ultimately making farming tasks easier, financially safer, and more efficient for farmers.",
+    name: "Streamlit track winner at RevaHack 2023",
+    title: "RevaHack 2023",
+  },
+  {
+    quote:
+      "The project is focused on improving customer support with Ludwig and the Hugging Face Mistral model. The objective is to fine-tune a large language model with Ludwig for precise intent classification using support call transcripts. By automating the categorization of customer queries, the fine-tuned model enables quick and targeted responses. Configured for optimal performance, the model aims to streamline customer support workflows, enhancing overall customer service efficiency.",
+    name: "Ludwig Hackathon Winners",
+    title: "Ludwig 10k Stars LLM Fine-tuning Hackathon Winners",
+  },
+  {
+    quote: "",
+    name: "IISC Pravega Cognavi Ascend Finalist Top-10",
+    title: "IISC Pravega Cognavi Ascend",
+  },
+  {
+    quote:
+      "Design Head for IEEE-RITB club with over 200+ student members. Actively involved in development and maintenance of the club website and other technical aspects",
+    name: "Digital Design Head",
+    title: "IEEE RIT-B",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Flutter Intern - AIE Tech",
+    desc: "Developed a feature-rich online education mobile app using Flask and Flutter, enabling seamless enrollment and interactive learning experiences.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Full Stack Developer - KGP",
+    desc: "Seamlessly integrated REST API with the frontend, ensuring smooth data exchange and optimal user experience.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Samung Prism Intern",
+    desc: "Worked with the dev of a ML on a project, from initial concept to deployment on GitHub.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Looking forward ....",
+    desc: "Seeking opportunities to leverage my expertise in building innovative and impactful solutions.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/abhishek09827",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/abhishek-kaushik-0a6a16243/",
+  },
+];
